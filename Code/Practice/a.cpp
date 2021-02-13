48939d708ac66efd495fd1ba868dd758be65e37e
Rishab Kulkarni <rishabkulkarni1703@gmail.com>
2021-02-13 19:15:47 +0530

    Added debug

--- Code/Practice/a.cpp
+++ Code/Practice/a.cpp
@@ -2,19 +2,46 @@
 using namespace std;
 typedef long long ll;
 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "[ " << #__VA_ARGS__ << " ]:", dbg_out(__VA_ARGS__)
+
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	
-	ll n;
-	cin >> n;
-	
-	for(int i = 1 ; ; i++){
-		if(n <= i){
-			cout << n << endl;
-			break;
+	int t;
+	cin >> t;
+	while(t--){
+		ll a,b;
+		cin >> a >> b;
+		
+		dbg(a,b);
+		break;
+		
+//		if(a == b){
+//			cout << 2 << endl;
+//			continue;
+//		}
+//		
+//		else if(b > a){
+//			cout << 1 << endl;
+//			continue;
+//		}
+		
+		bool ok = true;
+		int cnt = 0;
+		for(int i = 0 ; ; i++){
+			
+			(b == 1) ? (b++) : (a /= b) ;
+			cnt++;
+			
+			if(a == b || a < b || a == 0){
+				cnt++;
+				break;
+			}
 		}
-		n -= i;
+		cout << cnt + 1 << endl;
 	}
-	
 }
---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void dbg_out() { cerr << endl; }		// Credits - https://codeforces.com/profile/neal
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "[ " << #__VA_ARGS__ << " ]:", dbg_out(__VA_ARGS__)
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+}
